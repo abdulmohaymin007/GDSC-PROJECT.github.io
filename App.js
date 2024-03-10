e21b11511d9ad6be15198df95861cadcd5b2d76d
Abdul Mohaymin <142340734+abdulmohaymin007@users.noreply.github.com>
2024-03-10 14:41:23 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import News from './components/News/News'
+
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <News/>
+    </>
+  )
+}
+
+export default App
